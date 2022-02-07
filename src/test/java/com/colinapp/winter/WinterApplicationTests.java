6ea7a150ad989ffdf06d69a49298939f4a846822
colinwps <colinapp@hotmail.com>
2022-02-07 16:38:52 +0800

    整合redis

--- src/test/java/com/colinapp/winter/WinterApplicationTests.java
+++ src/test/java/com/colinapp/winter/WinterApplicationTests.java
@@ -1,13 +1,28 @@
 package com.colinapp.winter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.Date;
 
 @SpringBootTest
 class WinterApplicationTests {
+    @Autowired
+    RedisTemplate redisTemplate;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    //缓存测试
     @Test
     void contextLoads() {
+        ValueOperations<String,String> ops = stringRedisTemplate.opsForValue();
+        ops.set("name","colin");
+        ops.set("date",new Date().toString());
     }
+
 
 }
