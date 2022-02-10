d71b1e28704763044b9953ad163344a88b010b9b
colinwps <colinapp@hotmail.com>
2022-02-10 10:54:45 +0800

    集成Swagger

---
+++ src/main/java/com/colinapp/winter/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.colinapp.winter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Winter API Doc")
+                .description("this is a restful api document of colinapp.winter")
+                .version("1.0.0")
+                .build();
+    }
+}
--- src/main/java/com/colinapp/winter/controller/UserController.java
+++ src/main/java/com/colinapp/winter/controller/UserController.java
@@ -2,20 +2,25 @@ package com.colinapp.winter.controller;
 
 import com.colinapp.winter.entity.User;
 import com.colinapp.winter.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "用户信息管理")
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "根据考勤号获取用户信息",notes = "")
     @RequestMapping(value = "getUser/{id}",method = RequestMethod.GET)
-    public User GetUser(@PathVariable int id){
+    public User GetUser(@ApiParam(value = "考勤号",required = true) @PathVariable int id){
         return  userService.getUser(id);
     }
 
